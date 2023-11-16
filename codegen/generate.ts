0adc2728c1d863f8341e8167f0aa4d3de7237639
PyryM <pkmatikainen@gmail.com>
2023-11-15 23:11:05 -0500

    things

--- codegen/generate.ts
+++ codegen/generate.ts
@@ -1,34 +1,8 @@
+import { readFileSync } from "fs";
+
 console.log("Hello via Bun!");
 
-const EXAMPLE = `
-typedef enum WGPUCompareFunction {
-  WGPUCompareFunction_Undefined = 0x00000000,
-  WGPUCompareFunction_Never = 0x00000001,
-  WGPUCompareFunction_Less = 0x00000002,
-  WGPUCompareFunction_LessEqual = 0x00000003,
-  WGPUCompareFunction_Greater = 0x00000004,
-  WGPUCompareFunction_GreaterEqual = 0x00000005,
-  WGPUCompareFunction_Equal = 0x00000006,
-  WGPUCompareFunction_NotEqual = 0x00000007,
-  WGPUCompareFunction_Always = 0x00000008,
-  WGPUCompareFunction_Force32 = 0x7FFFFFFF
-} WGPUCompareFunction WGPU_ENUM_ATTRIBUTE;
-
-typedef enum WGPUCompilationInfoRequestStatus {
-  WGPUCompilationInfoRequestStatus_Success = 0x00000000,
-  WGPUCompilationInfoRequestStatus_Error = 0x00000001,
-  WGPUCompilationInfoRequestStatus_DeviceLost = 0x00000002,
-  WGPUCompilationInfoRequestStatus_Unknown = 0x00000003,
-  WGPUCompilationInfoRequestStatus_Force32 = 0x7FFFFFFF
-} WGPUCompilationInfoRequestStatus WGPU_ENUM_ATTRIBUTE;
-
-typedef enum WGPUCompilationMessageType {
-  WGPUCompilationMessageType_Error = 0x00000000,
-  WGPUCompilationMessageType_Warning = 0x00000001,
-  WGPUCompilationMessageType_Info = 0x00000002,
-  WGPUCompilationMessageType_Force32 = 0x7FFFFFFF
-} WGPUCompilationMessageType WGPU_ENUM_ATTRIBUTE;
-`;
+const SRC = readFileSync("codegen/webgpu.h").toString("utf8");
 
 interface CEnumVal {
   name: string;
@@ -40,14 +14,17 @@ interface CEnum {
   entries: CEnumVal[];
 }
 
-function cleanup(s: string, prefix: string): string {
-  s = s.trim();
+function removePrefix(s: string, prefix: string): string {
   if (s.startsWith(prefix)) {
     s = s.slice(prefix.length);
   }
-  if (s.startsWith("_")) {
-    s = s.slice(1);
-  }
+  return s;
+}
+
+function cleanup(s: string, prefix: string): string {
+  s = s.trim();
+  s = removePrefix(s, prefix);
+  s = removePrefix(s, "_");
   return s;
 }
 
@@ -69,10 +46,107 @@ function findEnums(src: string): CEnum[] {
   return res;
 }
 
-const enums = findEnums(EXAMPLE);
-for (const { name, entries } of enums) {
-  console.log(name);
-  for (const entry of entries) {
-    console.log(`[${entry.name}], [${entry.val}]`);
+function pyName(ident: string): string {
+  return removePrefix(ident, "WGPU");
+}
+
+function sanitizeIdent(ident: string): string {
+  if (!ident.match(/^[a-zA-Z]/) || ident === "None") {
+    ident = "_" + ident;
   }
+  return ident;
+}
+
+function emitEnum(src: CEnum): string {
+  let frags: string[] = [`class ${pyName(src.name)}(IntEnum):`];
+  for (const { name, val } of src.entries) {
+    frags.push(`    ${sanitizeIdent(name)} = ${val}`);
+  }
+  return frags.join("\n");
+}
+
+const EXAMPLE = `
+typedef struct WGPUAdapterImpl* WGPUAdapter WGPU_OBJECT_ATTRIBUTE;
+typedef struct WGPUBindGroupImpl* WGPUBindGroup WGPU_OBJECT_ATTRIBUTE;
+typedef struct WGPUBindGroupLayoutImpl* WGPUBindGroupLayout WGPU_OBJECT_ATTRIBUTE;
+typedef struct WGPUBufferImpl* WGPUBuffer WGPU_OBJECT_ATTRIBUTE;
+typedef struct WGPUCommandBufferImpl* WGPUCommandBuffer WGPU_OBJECT_ATTRIBUTE;
+typedef struct WGPUCommandEncoderImpl* WGPUCommandEncoder WGPU_OBJECT_ATTRIBUTE;
+
+typedef struct WGPUAdapterProperties {
+    WGPUChainedStructOut * nextInChain;
+    uint32_t vendorID;
+    char const * vendorName;
+    char const * architecture;
+    uint32_t deviceID;
+    char const * name;
+    char const * driverDescription;
+    WGPUAdapterType adapterType;
+    WGPUBackendType backendType;
+} WGPUAdapterProperties WGPU_STRUCTURE_ATTRIBUTE;
+
+typedef struct WGPUBindGroupEntry {
+    WGPUChainedStruct const * nextInChain;
+    uint32_t binding;
+    WGPU_NULLABLE WGPUBuffer buffer;
+    uint64_t offset;
+    uint64_t size;
+    WGPU_NULLABLE WGPUSampler sampler;
+    WGPU_NULLABLE WGPUTextureView textureView;
+} WGPUBindGroupEntry WGPU_STRUCTURE_ATTRIBUTE;
+`;
+
+interface CStructEntry {
+  name: string;
+  type: string;
+}
+
+interface CStruct {
+  name: string;
+  rawBody: string;
+  entries: CStructEntry[]
+}
+
+function parseStructEntry(entry: string): CStructEntry {
+  const matched = entry.match(/(.*) ([A-Za-z0-9_]+)$/);
+  if(!matched) {
+    throw new Error(`Unable to parse: "${entry}"`);
+  }
+  const [_wholeMatch, type, name] = matched;
+  return {name, type}
+}
+
+function findConcreteStructs(src: string): CStruct[] {
+  const res: CStruct[] = [];
+  const reg = /typedef struct ([a-zA-Z0-9]*) \{([^\}]*)\}/g;
+  for (const m of src.matchAll(reg)) {
+    const [_wholeMatch, name, rawBody] = m;
+    const entries: CStructEntry[] = [];
+    for(const line of rawBody.split(";")) {
+      if(line.trim().length > 0) {
+        entries.push(parseStructEntry(line.trim()));
+      }
+    }
+    res.push({ name: name.trim(), rawBody, entries });
+  }
+  return res;
+}
+
+function findOpaquePointers(src: string): Set<string> {
+  throw new Error("NYI!");
+  const opaques: Set<string> = new Set();
+  const reg = /typedef struct [a-zA-Z0-9]*\* /g;
+  return opaques;
+}
+
+// const enums = findEnums(SRC);
+// for (const e of enums) {
+//   console.log(emitEnum(e));
+//   console.log("");
+// }
+
+const structs = findConcreteStructs(SRC);
+for (const s of structs) {
+  console.log(s.name);
+  console.log(s.entries.map((e) => `[${e.name}] [${e.type}]`).join("\n"))
 }
