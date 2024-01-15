73ee26588d2824a5f44c860fa2b5ce1756ac66e4
PyryM <pkmatikainen@gmail.com>
2024-01-15 17:05:04 -0500

    Update llm_doc_comments.ts

--- codegen/llm_doc_comments.ts
+++ codegen/llm_doc_comments.ts
@@ -3,26 +3,55 @@ import { readFileSync } from "fs";
 const src = readFileSync("webgoo/bindings.py").toString("utf8");
 let curClass: string | undefined = undefined;
 
-for(const line of src.split("\n")) {
-  if(line.startsWith("class ")) {
+let nextFuncIsProperty = false;
+
+interface DocThing {
+  parent?: string;
+  name: string;
+  kind: "class" | "func" | "prop";
+}
+
+const things: Map<string, DocThing> = new Map();
+
+for (const line of src.split("\n")) {
+  if (line.startsWith("class ")) {
     const matched = line.trim().match(/^class ([^(]*)(?:\([^)]*\))?:/);
-    if(!matched) {
+    if (!matched) {
       console.log("Missing match?", line);
-      continue
+      continue;
     }
     const [_match, className] = matched;
     curClass = className;
+    if (!things.has(className)) {
+      things.set(className, { name: className, kind: "class" });
+    }
+  } else if (line.trim().startsWith("@property")) {
+    nextFuncIsProperty = true;
   } else {
     const matched = line.match(/^(\s*)def ([^(]+)\(/);
-    if(!matched) {continue}
+    if (!matched) {
+      continue;
+    }
     const [_match, indent, funcname] = matched;
-    if(indent.length == 0) {
+    const isProp = nextFuncIsProperty;
+    nextFuncIsProperty = false;
+    if (indent.length == 0) {
       curClass = undefined;
     }
-    if(curClass?.startsWith("_") || funcname.startsWith("_")) {
+    if (curClass?.startsWith("_") || funcname.startsWith("_")) {
       continue;
     }
     const name = curClass === undefined ? funcname : `${curClass}.${funcname}`;
-    console.log("Found:", name);
+    if (!things.has(name)) {
+      things.set(name, {
+        kind: isProp ? "prop" : "func",
+        parent: curClass,
+        name: funcname,
+      });
+    }
   }
+}
+
+for (const [name, thing] of things.entries()) {
+  console.log(name, "->", thing.name, thing.kind);
 }
