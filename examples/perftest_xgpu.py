8f969cb2c9de310a58e4fd9588c8204817acca62
PyryM <pkmatikainen@gmail.com>
2024-01-24 14:26:13 -0500

    shaders: use predeclared type aliases

--- examples/perftest_xgpu.py
+++ examples/perftest_xgpu.py
@@ -25,30 +25,30 @@ def set_transform(target: NDArray, rot, scale: float, pos: NDArray):
 
 SHADER_SOURCE = """
 struct GlobalUniforms {
-  @align(16) view_proj_mat: mat4x4<f32>
+  @align(16) view_proj_mat: mat4x4f
 }
 struct DrawUniforms {
-  @align(16) model_mat: mat4x4<f32>,
-  @align(16) color: vec4<f32>,
+  @align(16) model_mat: mat4x4f,
+  @align(16) color: vec4f,
 }
 @group(0) @binding(0) var<uniform> global_uniforms: GlobalUniforms;
 @group(1) @binding(0) var<uniform> draw_uniforms: DrawUniforms;
 struct VertexInput {
-    @location(0) pos: vec4<f32>,
+    @location(0) pos: vec4f,
 };
 struct VertexOutput {
-    @builtin(position) pos: vec4<f32>,
-    @location(0) color : vec4<f32>,
+    @builtin(position) pos: vec4f,
+    @location(0) color : vec4f,
 };
 @vertex
 fn vs_main(in: VertexInput) -> VertexOutput {
-    let world_pos = draw_uniforms.model_mat * vec4<f32>(in.pos.xyz, 1.0f);
+    let world_pos = draw_uniforms.model_mat * vec4f(in.pos.xyz, 1.0f);
     let clip_pos = global_uniforms.view_proj_mat * world_pos;
-    let color = draw_uniforms.color * clamp(in.pos, vec4<f32>(0.0f), vec4<f32>(1.0f));
+    let color = draw_uniforms.color * clamp(in.pos, vec4f(0.0f), vec4f(1.0f));
     return VertexOutput(clip_pos, color);
 }
 @fragment
-fn fs_main(in: VertexOutput) -> @location(0) vec4<f32> {
+fn fs_main(in: VertexOutput) -> @location(0) vec4f {
     return in.color;
 }
 """
--- examples/triangle_windowed.py
+++ examples/triangle_windowed.py
@@ -17,33 +17,33 @@ struct VertexInput {
     @builtin(vertex_index) vertex_index : u32,
 };
 struct VertexOutput {
-    @location(0) color : vec4<f32>,
-    @builtin(position) pos: vec4<f32>,
+    @location(0) color : vec4f,
+    @builtin(position) pos: vec4f,
 };
 
 @vertex
 fn vs_main(in: VertexInput) -> VertexOutput {
-    var positions = array<vec2<f32>, 3>(
-        vec2<f32>(0.0, -0.5),
-        vec2<f32>(0.5, 0.5),
-        vec2<f32>(-0.5, 0.75),
+    var positions = array<vec2f, 3>(
+        vec2f(0.0, -0.5),
+        vec2f(0.5, 0.5),
+        vec2f(-0.5, 0.75),
     );
-    var colors = array<vec3<f32>, 3>(  // srgb colors
-        vec3<f32>(1.0, 1.0, 0.0),
-        vec3<f32>(1.0, 0.0, 1.0),
-        vec3<f32>(0.0, 1.0, 1.0),
+    var colors = array<vec3f, 3>(  // srgb colors
+        vec3f(1.0, 1.0, 0.0),
+        vec3f(1.0, 0.0, 1.0),
+        vec3f(0.0, 1.0, 1.0),
     );
     let index = i32(in.vertex_index);
     var out: VertexOutput;
-    out.pos = vec4<f32>(positions[index], 0.0, 1.0);
-    out.color = vec4<f32>(colors[index], 1.0);
+    out.pos = vec4f(positions[index], 0.0, 1.0);
+    out.color = vec4f(colors[index], 1.0);
     return out;
 }
 
 @fragment
-fn fs_main(in: VertexOutput) -> @location(0) vec4<f32> {
-    let physical_color = pow(in.color.rgb, vec3<f32>(2.2));  // gamma correct
-    return vec4<f32>(physical_color, in.color.a);
+fn fs_main(in: VertexOutput) -> @location(0) vec4f {
+    let physical_color = pow(in.color.rgb, vec3f(2.2));  // gamma correct
+    return vec4f(physical_color, in.color.a);
 }
 """
 
