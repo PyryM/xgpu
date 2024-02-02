bb7a33df7d4b95deb00e45ef403f1c5f240b86d4
Michael Dawson-Haggerty <mikedh@kerfed.com>
2024-02-02 15:58:02 -0500

    there we go passing again

--- xgpu/extensions/__init__.py
+++ xgpu/extensions/__init__.py
@@ -1,9 +1,10 @@
-from .helpers import get_device, startup
+from .helpers import get_device, startup, enable_logging
 from .wrappers import BinderBuilder, XDevice, XSurface, auto_vertex_layout
 
 __all__ = [
     "get_device",
     "startup",
+    "enable_logging",
     "BinderBuilder",
     "XDevice",
     "XSurface",
