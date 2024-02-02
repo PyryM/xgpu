0d7ee03b680abf1fb22f9f3fd821c9d3896dc63c
Michael Dawson-Haggerty <mikedh@kerfed.com>
2024-02-02 16:03:32 -0500

    make include directory in fetch script

--- codegen/fetch_wgpu_bins.py
+++ codegen/fetch_wgpu_bins.py
@@ -103,6 +103,11 @@ def unzip_to(url: str, dest: str):
 
 
 UNZIP_PATH = "wgpu_native_unzipped"
+INCLUDE_PATH = "xgpu/include"
+
+# make sure the include path exists
+os.makedirs(INCLUDE_PATH, exist_ok=True)
+
 if OS != "macos":
     unzip_to(make_url(OS, ARCH), UNZIP_PATH)
 else:
@@ -116,8 +121,8 @@ LIBS = [
 ]
 
 COPIES = [
-    (f"{UNZIP_PATH}/webgpu.h", "xgpu/include/webgpu.h"),
-    (f"{UNZIP_PATH}/wgpu.h", "xgpu/include/wgpu.h"),
+    (f"{UNZIP_PATH}/webgpu.h", f"{INCLUDE_PATH}/webgpu.h"),
+    (f"{UNZIP_PATH}/wgpu.h", f"{INCLUDE_PATH}/wgpu.h"),
     *LIBS,
 ]
 
